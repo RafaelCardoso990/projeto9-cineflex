b2895984bbd3b843bcce6be3d30791a4e93e6614
Rafael Ferreira Cardoso <93937958+RafaelCardoso990@users.noreply.github.com>
2022-03-28 17:50:26 -0300

    Delete projeto7-instagram-react/projeto7-instagram-react directory

--- projeto7-instagram-react/projeto7-instagram-react/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom";
-import App from "./App";
-// implemente a página utilizando as funções do react
-const app = App();
-
-const elemento = document.querySelector(".root");
-ReactDOM.render(app, elemento);
-
